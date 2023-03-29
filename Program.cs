d04757cd67b41b648a2704791bf0a9071a362725
Dp7611 <DP761@student.aru.ac.uk>
2023-03-29 17:10:22 +0100

    Added basic pizza list

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿namespace DPPizza;
 class Program
 {
+
+        
+
     static void Main(string[] args)
     {
         string username;
-        
-        
+        string pizzatypename;
+
         Console.WriteLine("=================================");
         Console.WriteLine("=====  Welcome to DPPizza!  =====");
         Console.WriteLine("=================================");
         Console.WriteLine("Please type your fist name and press enter:");
         username = Console.ReadLine();
-        Console.WriteLine($"Thank you {username}, please select your pizza from list below (1/2/3/4):");
+        Console.WriteLine($"Thank you {username}, please select your pizza from list below:");
+        Console.WriteLine("Margherita");
+        Console.WriteLine("Pepperoni");
+        Console.WriteLine("Vegetarian");
+        pizzatypename = Console.ReadLine();
+        Console.WriteLine($"Thank you {username}, you have selected {pizzatypename}");
+        Console.WriteLine("Please confirm if this is the correct pizza for you today (y/n):");
         Console.ReadKey(true);
     }
 }
