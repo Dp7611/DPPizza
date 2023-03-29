aeadac02f38a12856998347739869b507be64fbf
Dp7611 <DP761@student.aru.ac.uk>
2023-03-29 16:15:09 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DPPizza;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username;
+        
+        
+        Console.WriteLine("=================================");
+        Console.WriteLine("=====  Welcome to DPPizza!  =====");
+        Console.WriteLine("=================================");
+        Console.WriteLine("Please type your fist name and press enter:");
+        username = Console.ReadLine();
+        Console.WriteLine($"Thank you {username}, please select your pizza from list below (1/2/3/4):");
+        Console.ReadKey(true);
+    }
+}
